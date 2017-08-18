0fe2770202c0482839694f88469f9cd4a9af98a8
Marc Edwards <mdouglasedwards@gmail.com>
2017-08-18 16:33:49 -0400

    Initial setup of project

---
+++ controllers/authentication.js
@@ -0,0 +1,39 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+
+function tokenForUser(user) {
+	const timestamp = new Date().getTime();
+	// 'sub' = 'subject', 'iat' = 'issued at time'
+	return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function(req, res, next) {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	if (!email || !password) return res.status(422).send({ error: 'You must provide email and password' });
+
+	// See if a user with the given email exists
+	User.findOne({ email: email }, function(err, existingUser) {
+		if (err) return next(err);
+
+		// If a user with email does exist, then return an error
+		if (existingUser) return res.status(422).send({ error: 'Email is in use' });
+
+		// If a user with email does not exist, then create and save user record
+		const user = new User({
+			email: email,
+			password: password
+		});
+
+		user.save(function(err) {
+			if (err) return next(err);
+
+			// Respond to request indicating the user was createed
+			res.json({ token: tokenForUser(user) }});
+		});
+
+	});
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+// Main starting point for application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// Db setup
+mogoose.connect('mongodb://localhost:auth/auth'); // creates an 'auth' database
+
+// App setup
+const app = express();
+// load our middlewares
+app.use(morgan('combined')); // logs http requests
+app.use(bodyParser.json({ type: '*/*' })); // helps parse incoming http requests
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port: ', port);
